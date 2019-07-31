081bf9f343b7f34068ac3a5bfc3dd23a8ba4ff55
mayonglin <mayonglin@sf-express.com>
2019-07-31 16:29:04 +0800

    1.打印http日志

--- server/src/main/java/com/abc/config/RestTemplateConfig.java
+++ server/src/main/java/com/abc/config/RestTemplateConfig.java
@@ -3,6 +3,8 @@ package com.abc.config;
 import com.abc.util.http.LoggingClientHttpRequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
@@ -15,7 +17,7 @@ public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        RestTemplate restTemplate = new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate(new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()));
         restTemplate.getInterceptors().add(new LoggingClientHttpRequestInterceptor());
 
         List<HttpMessageConverter<?>> list = restTemplate.getMessageConverters();
