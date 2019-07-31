236515fed4994f91a993c05ffd9382536ae31477
mayonglin <mayonglin@sf-express.com>
2019-07-31 16:16:37 +0800

    1.解決乱码问题 2.打印http日志

--- server/src/main/java/com/abc/config/RestTemplateConfig.java
+++ server/src/main/java/com/abc/config/RestTemplateConfig.java
@@ -1,8 +1,14 @@
 package com.abc.config;
 
+import com.abc.util.http.LoggingClientHttpRequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @Configuration
 public class RestTemplateConfig {
@@ -10,6 +16,15 @@ public class RestTemplateConfig {
     @Bean
     public RestTemplate restTemplate() {
         RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getInterceptors().add(new LoggingClientHttpRequestInterceptor());
+
+        List<HttpMessageConverter<?>> list = restTemplate.getMessageConverters();
+        for (HttpMessageConverter<?> httpMessageConverter : list) {
+            if (httpMessageConverter instanceof StringHttpMessageConverter) {
+                ((StringHttpMessageConverter) httpMessageConverter).setDefaultCharset(StandardCharsets.UTF_8);
+                break;
+            }
+        }
         return restTemplate;
     }
 }
