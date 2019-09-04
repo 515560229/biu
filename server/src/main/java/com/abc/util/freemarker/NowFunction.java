9ce1168a4f90d4db23c6a0bda5ce7caf4f427316
mayonglin <mayonglin@sf-express.com>
2019-09-04 16:23:47 +0800

    1.支持$(now("timestamp"))

--- server/src/main/java/com/abc/util/freemarker/NowFunction.java
+++ server/src/main/java/com/abc/util/freemarker/NowFunction.java
@@ -1,7 +1,6 @@
 package com.abc.util.freemarker;
 
 import com.abc.exception.MessageRuntimeException;
-import freemarker.template.TemplateMethodModelEx;
 import freemarker.template.TemplateModel;
 
 import java.text.SimpleDateFormat;
@@ -24,12 +23,22 @@ public class NowFunction extends CustomFunction {
         return DESC;
     }
 
+    /**
+     * $(now()) return 20190101210000232
+     * $(now('yyyy-MM-dd HH:mm:ss')) return 2019-01-01 21:00:00
+     * $(now('timestamp')) return 1546347600232
+     * @param paramList
+     * @return
+     */
     @Override
     public String execFunction(List paramList) {
         if (paramList.size() > 1) {
             throw new MessageRuntimeException("参数个数不能>=2");
         }
         if (paramList.size() == 1) {
+            if (paramList.get(0).equals("timestamp")) {
+                return String.valueOf(new Date().getTime());
+            }
             return new SimpleDateFormat(FreemarkerUtils.getObject(((TemplateModel) paramList.get(0))).toString())
                     .format(new Date());
         }
