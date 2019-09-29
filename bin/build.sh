70a2cbbb554d4af5511ff157b789cc24e16d2e49
mayonglin <mayonglin@sf-express.com>
2019-09-29 16:53:10 +0800

    新增markdown文档功能

--- bin/build.sh
+++ bin/build.sh
@@ -2,5 +2,5 @@ cd /Users/618715/program/github-git/biu
 cd browser
 npm run build:prod
 cd ../server
-mvn package -Dmaven.test.skip=true
+mvn clean package -Dmaven.test.skip=true
 scp /Users/618715/program/github-git/biu/server/target/biu-1.0.jar appdeploy@10.203.14.137:/app/war/biu
