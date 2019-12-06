974c3ba9e9e4b45a6bc927e8d898b2d90b8533a7
Ivan.Ma <515560229@qq.com>
2019-12-06 23:15:09 +0800

    支持将函数视为非参数。这可以适用有些参数是由函数自动生成的场景。

--- browser/src/utils/templateParser.js
+++ browser/src/utils/templateParser.js
@@ -4,14 +4,18 @@
  * @returns {Array}
  */
 export function getParameters(str) {
-  let g = /\$\{(.*?)\}/g;
+  let g = /\$\{(.*?)\}/g;//匹配${}
+  let funReg = /^.*\(.*\)$/;//函数匹配
   let group = str.match(g);
   if (group) {
     let arr = [];
     for (let idx in group) {
       const item = group[idx];
       const val = item.substring(2, item.length - 1).trim();
-      arr.splice(idx, 1, val)
+      if (!val.match(funReg)) {
+        //不是函数才加进参数列表，涵数不是参数，由后台模板渲染
+        arr.splice(idx, 1, val);
+      }
     }
     return arr;
   }
